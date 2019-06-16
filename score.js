0a6a62a266b7e33c3107a255133a36afcce8b19a
Qasim Laghari <go.jets.laghari@gmail.com>
2019-06-15 17:31:36 -0700

    Setup k-nearest neighbors

--- score.js
+++ score.js
@@ -1,4 +1,6 @@
 const outputs = [];
+const predictionPoint = 300;
+const k = 3;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   // Ran every time a balls drops into a bucket
@@ -6,6 +8,14 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   console.log(outputs);
 }
 
+function distance(point) {
+  return Math.abs(point - predictionPoint);
+}
+
 function runAnalysis() {
   // Write code here to analyze stuff
+  _.chain(outputs)
+    .map(row => [distance(row[0]), row[3]])
+    .sortBy(row => row[0])
+    .slice(0, k);
 }
